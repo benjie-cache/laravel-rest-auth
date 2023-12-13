e9d6b8886fd3f73d685484b2160b4e71dd302556
benjie-cache <benngovi47@gmail.com>
2023-12-13 13:35:43 +0300

    'v1.0.0.1'

---
+++ routes/api.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProductsController;
+use App\Http\Controllers\UserAuthenticationController;
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+
+//These routes are NOT protected using middleware
+
+Route::prefix('v1')->group(function () {
+    Route::post('login', [UserAuthenticationController::class, 'login']);
+    Route::post('register', [UserAuthenticationController::class, 'register']);
+});
+
+
+//These routes are protected using middleware
+Route::prefix('v1')->middleware('auth:sanctum')->group(function () {
+    Route::post('logout', [UserAuthenticationController::class, 'logout']);
+    Route::apiResource('products', ProductsController::class);
+});
